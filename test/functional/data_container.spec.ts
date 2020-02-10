31fde61cd598c676e09683d74361c43a4bfb75a7
Robert Kania <cezaryrk@yahoo.de>
2020-02-10 21:58:06 +0100

    test: refactor of treeutils

--- test/functional/data_container.spec.ts
+++ test/functional/data_container.spec.ts
@@ -1,23 +1,24 @@
-import {suite, test} from "mocha-typescript";
-import {expect} from "chai";
+import {suite, test} from 'mocha-typescript';
+import {expect} from 'chai';
 
 
-import * as _ from "lodash";
-import {ValidPerson} from "./schemas/validation/ValidPerson";
-import {EntityRegistry} from "../../src";
-import {DataContainer, TreeUtils, WalkValues} from "@typexs/base";
+import * as _ from 'lodash';
+import {ValidPerson} from './schemas/validation/ValidPerson';
+import {EntityRegistry} from '../../src';
+import {DataContainer} from '@typexs/base';
+import {TreeUtils, WalkValues} from 'commons-base';
 
 const registry = EntityRegistry.$();
 
 @suite('functional/data_container')
-class Data_containerSpec {
+class DataContainerSpec {
 
 
   @test
   async 'check empty'() {
-    let p = new ValidPerson();
-    let c = new DataContainer(p, registry);
-    let retCode = await c.validate();
+    const p = new ValidPerson();
+    const c = new DataContainer(p, registry);
+    const retCode = await c.validate();
     expect(retCode).to.be.false;
     expect(c.hasErrors()).to.be.true;
     expect(c.errors).to.have.length(3);
@@ -28,10 +29,10 @@ class Data_containerSpec {
 
   @test
   async 'half filled'() {
-    let p = new ValidPerson();
+    const p = new ValidPerson();
     p.lastName = 'Blacky';
-    let c = new DataContainer(p, registry);
-    let retCode = await c.validate();
+    const c = new DataContainer(p, registry);
+    const retCode = await c.validate();
     expect(retCode).to.be.false;
     expect(c.hasErrors()).to.be.true;
     expect(c.errors).to.have.length(2);
@@ -42,12 +43,12 @@ class Data_containerSpec {
 
   @test
   async 'full filled'() {
-    let p = new ValidPerson();
+    const p = new ValidPerson();
     p.firstName = 'Funny';
     p.lastName = 'Blacky';
     p.eMail = 'world@warcraft.tv';
-    let c = new DataContainer(p, registry);
-    let retCode = await c.validate();
+    const c = new DataContainer(p, registry);
+    const retCode = await c.validate();
     expect(retCode).to.be.true;
     expect(c.hasErrors()).to.be.false;
     expect(c.errors).to.have.length(0);
@@ -58,12 +59,12 @@ class Data_containerSpec {
 
   @test
   async 'dummy full filled'() {
-    let regEntityDef = registry.getEntityRefFor('ValidPerson');
-    let data = regEntityDef.toJson();
-    let data_x = JSON.parse(JSON.stringify(data));
+    const regEntityDef = registry.getEntityRefFor('ValidPerson');
+    const data = regEntityDef.toJson();
+    const data_x = JSON.parse(JSON.stringify(data));
 
     TreeUtils.walk(data_x, (v: WalkValues) => {
-      if (v.value == 'ValidPerson') {
+      if (v.value === 'ValidPerson') {
         v.parent[v.key] = 'ValidPerson2';
       } else if (_.isString(v.value) && /valid_person/.test(v.value)) {
         v.parent[v.key] = v.value.replace('valid_person', 'valid_person_2');
@@ -72,15 +73,15 @@ class Data_containerSpec {
     });
     data_x.machineName = 'valid_person_2';
 
-    let entityDef2 = registry.fromJson(data_x);
+    const entityDef2 = registry.fromJson(data_x);
 
-    let p:any = entityDef2.create();
+    const p: any = entityDef2.create();
     p.firstName = 'Funny';
     p.lastName = 'Blacky';
     p.eMail = 'world@warcraft.tv';
 
-    let c = new DataContainer(p, registry);
-    let retCode = await c.validate();
+    const c = new DataContainer(p, registry);
+    const retCode = await c.validate();
     expect(retCode).to.be.true;
     expect(c.hasErrors()).to.be.false;
     expect(c.errors).to.have.length(0);
